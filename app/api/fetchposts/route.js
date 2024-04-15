b6be9e6a02ba452869be55fdccfb090f89131547
adarshswaminath <adarshswaminath7@gmail.com>
2024-04-15 13:48:32 +0530

    feat: scrap all new blogs from the dev.to

---
+++ app/api/fetchposts/route.js
@@ -0,0 +1,35 @@
+import { NextResponse } from "next/server";
+import puppeteer from "puppeteer";
+
+export const GET = async (req, res) => {
+    try {
+        const website_url = "https://dev.to/";
+        const browser = await puppeteer.launch({ headless: true });
+        const page = await browser.newPage();
+        await page.goto(website_url);
+
+        const results = await page.evaluate(() => {
+            const elements = document.querySelectorAll(".crayons-story__indention .crayons-story__title a");
+            
+            const data = Array.from(elements).map(element => ({
+                textContent: element.textContent.trim(),
+                href: element.getAttribute('href')
+            }));
+            return data;
+        });
+
+        console.log(results);
+
+        await browser.close();
+
+        return NextResponse.json({ data: results });
+    } catch (error) {
+        console.log(error.message);
+        return NextResponse.json({ error: error.message });
+    }
+};
+
+export const POST = async (req, res) => {
+    const { id } = await req.json();
+    return NextResponse.json({ res: "POST Request" });
+};
