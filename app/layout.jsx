4c953b8d3ac259cd219c212350f12b7a9cecc3de
adarshswaminath <adarshswaminath7@gmail.com>
2024-04-16 09:58:33 +0530

    remove: all fron end config

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,5 @@
-import { Inter } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Blog2Tweet: Automating Blog Discovery and Tweet Creation",
@@ -10,8 +8,8 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" data-theme="dark">
+      <body>{children}</body>
     </html>
   );
 }
