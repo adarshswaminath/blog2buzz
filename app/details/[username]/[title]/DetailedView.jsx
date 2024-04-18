64f24c1b3282d0fcfa3316349a1268213be2a0a3
adarshswaminath <adarshswaminath7@gmail.com>
2024-04-18 22:48:13 +0530

    added: save blog component added need to implment the functionality

--- app/details/[username]/[title]/DetailedView.jsx
+++ app/details/[username]/[title]/DetailedView.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import Image from 'next/image'
 import AIContentGeneration from './AIContentGeneration'
+import SaveThePost from './SaveThePost'
+
+
 export default function DetailedView({ response, article_url }) {
   const { imageUrl, title, article } = response
+  console.table(response)
   return (
     <div>
       {imageUrl && <Image src={imageUrl} alt='Image Not found'
@@ -14,10 +18,12 @@ export default function DetailedView({ response, article_url }) {
         <div className="mt-12 w-full">
           <h2 className="text-2xl font-bold max-w-2xl">{title}</h2>
           <p className='text-justify mt-4'>{article.slice(0, 512)}.....</p>
-          <div className="mt-4 flex justify-end">
+          <div className="mt-4 flex justify-between">
             <a href={`https://dev.to/${article_url}`} target='_blank'>
               <button className='btn text-white bg-gradient-to-br from-[#248AE9] to-purple hover:bg-purple font-bold'>Read Full Article</button>
             </a>
+           {/* // ?save the post */}
+           <SaveThePost response={response}/>
           </div>
         </div>
          {/* Ai Content */}
