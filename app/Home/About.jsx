22e53c7d6e280727476a556afcae7182b3f4d56a
adarshswaminath <adarshswaminath7@gmail.com>
2024-04-21 16:22:30 +0530

    feat:home page mobile view updates

--- app/Home/About.jsx
+++ app/Home/About.jsx
@@ -5,10 +5,10 @@ import { FaGithub } from 'react-icons/fa6'
 
 export default function About() {
     return (
-        <div className="w-[calc(100%-4rem)] mx-auto rounded-xl overflow-hidden">
+        <div className="w-full lg:w-[calc(100%-4rem)] mx-auto rounded-xl overflow-hidden">
             <Vortex
                 backgroundColor="#e90df3"
-                className="flex items-center flex-col justify-center px-2 md:px-10 py-4 w-full h-full bg-transparent"
+                className="flex items-center flex-col justify-center px-2 md:px-10 py-4 w-full h-full bg-transparent rounded-xl"
             >
                 <h2 className="text-white text-xl  font-bold text-center">
                     About the Project!
@@ -16,12 +16,18 @@ export default function About() {
                 <p className="text-white text-lg font-medium mt-6 text-justify">
                     If you're a developer, student, security specialist, or anyone involved in the tech industry, staying updated with current market technology is crucial. However, it can be a challenging task. Even though there are free platforms like Medium, dev.to, and DailyDev available, many of us struggle to find the time to explore blogs and learn more about emerging technologies. Some of us might even admit to being a bit lazy when it comes to this. For those facing this issue, Blog2Buzz offers the perfect solution. Here, you can access the latest blogs from dev.to. With the assistance of AI (Google Gemini), you can summarize content or create Twitter posts and Medium write-ups. Just like learning, sharing knowledge is equally important. By creating Twitter posts or Medium articles, you're effectively disseminating knowledge to others. This platform not only helps you save time but also facilitates knowledge sharing.
                 </p>
-                <a href="" className='mt-5'>
+                <div className="hidden lg:flex justify-center">
+                <a href="">
                     <button className="btn bg-white text-black hover:text-white px-12 rounded-full">View Project In <FaGithub className="text-xl" /></button>
-
                 </a>
+            </div>
 
             </Vortex>
+            <div className="lg:hidden flex justify-center mt-8">
+                <a href="">
+                    <button className="btn bg-white text-black hover:text-white px-12 rounded-full">View Project In <FaGithub className="text-xl" /></button>
+                </a>
+            </div>
         </div>
 
     )
