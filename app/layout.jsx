e3ecdcfa65c9cc35a0ff6154268e6d8ebd225685
adarshswaminath <adarshswaminath7@gmail.com>
2024-04-21 18:07:38 +0530

    feat: improve meta title

--- app/layout.jsx
+++ app/layout.jsx
@@ -6,7 +6,7 @@ import { CSPostHogProvider } from "./provider";
 
 export const metadata = {
   title: "Blog2Buzz: Automating Blog Discovery and Tweet Creation",
-  description: "Blog2Buzz simplifies the task of discovering blog content on Dev.to and converting it into tweet-ready posts using AI. Effortlessly browse, select, and share trending topics with your followers on Twitter",
+  description: "Blog2Buzz: Stay Updated with Tech Trends | AI-Powered Blog Summarization & Social Sharing",
 };
 
 export default function RootLayout({ children }) {
