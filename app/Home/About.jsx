d686fdf58e4380f69cff25373712225052bab804
adarshswaminath <adarshswaminath7@gmail.com>
2024-04-21 18:50:49 +0530

    feat: Implemented direct scraping and delivery of dev.to blog data as response

--- app/Home/About.jsx
+++ app/Home/About.jsx
@@ -16,18 +16,18 @@ export default function About() {
                 <p className="text-white text-lg font-medium mt-6 text-justify">
                 For developers, students, security specialists, and anyone in the tech industry, staying updated with current market technology is crucial. However, finding the time to explore blogs and learn about emerging technologies can be challenging. Blog2Buzz provides the ideal solution for those facing this issue. Utilizing web scraping, it offers access to the latest dev.to blogs and harnesses AI assistance to summarize content or craft engaging Twitter posts and Medium articles. Just like learning, sharing knowledge is equally vital. By creating Twitter posts or Medium articles, you effectively disseminate knowledge to others. Blog2Buzz not only saves time but also facilitates knowledge sharing seamlessly.
                 </p>
-                <div className="hidden lg:flex justify-center">
+                <div className="flex justify-center mt-8">
                     <a href="">
                         <button className="btn bg-white text-black hover:text-white px-12 rounded-full">View Project In <FaGithub className="text-xl" /></button>
                     </a>
                 </div>
 
             </Vortex>
-            <div className="lg:hidden md:flex flex justify-center mt-10">
+            {/* <div className="lg:hidden md:flex flex justify-center mt-10">
                 <a href="">
                     <button className="btn bg-white text-black hover:text-white px-12 rounded-full">View Project In <FaGithub className="text-xl" /></button>
                 </a>
-            </div>
+            </div> */}
         </div>
 
     )
