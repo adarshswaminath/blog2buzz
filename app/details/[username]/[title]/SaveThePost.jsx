ca1db4966fd6252655e5c282034a749392100db0
adarshswaminath <adarshswaminath7@gmail.com>
2024-04-21 17:12:43 +0530

    feat: implement transition in save page

--- app/details/[username]/[title]/SaveThePost.jsx
+++ app/details/[username]/[title]/SaveThePost.jsx
@@ -1,37 +1,35 @@
 "use client"
-
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import { FaRegBookmark } from "react-icons/fa";
 import { IoMdBookmark } from "react-icons/io";
-function SaveThePost({dataToSave}) {
-    const [isSaved,setIsSaved] = useState(false)
-    
+
+function SaveThePost({ dataToSave }) {
+    const [isSaved, setIsSaved] = useState(false);
+
     const savePost = () => {
         setIsSaved(true);
         
-        //? Retrieve existing posts array from local storage or initialize it if not exists
         let posts = JSON.parse(localStorage.getItem("posts")) || [];
-        //? Add the new post to the beginning of the array (LIFO order)
         posts.unshift(dataToSave);
-        //? Save the updated array back to the local storage
         localStorage.setItem("posts", JSON.stringify(posts));
+
         setTimeout(() => {
             setIsSaved(false);
-        }, 900);
+        }, 2000); // Change the timeout duration if needed
     }
-    
-  return (
-    <button onClick={savePost} className="btn border-none bg-transparent">
-        {isSaved ? (
-            <span className='flex items-center gap-x-2 text-base'> <IoMdBookmark className='text-2xl text-purple animate-bounce'/> Saved</span>
 
-        ) : (
+    return (
+        <button onClick={savePost} className="btn border-none bg-transparent">
             <span className='flex items-center gap-x-2 text-base'>
-                <FaRegBookmark/> Save
+                {isSaved ? (
+                    <IoMdBookmark className='text-2xl text-purple animate-pulse'/> 
+                ) : (
+                    <FaRegBookmark/> 
+                )}
+                {isSaved ? "Saved" : "Save"}
             </span>
-        )}
-    </button>
-  )
+        </button>
+    );
 }
 
-export default SaveThePost
+export default SaveThePost;
