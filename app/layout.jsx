73a193e7ced05a54400549a9d43c220dfc712f47
adarshswaminath <adarshswaminath7@gmail.com>
2024-04-21 16:36:34 +0530

    feat: font update #1

--- app/layout.jsx
+++ app/layout.jsx
@@ -11,10 +11,15 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" data-theme="dark">
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300..700&display=swap" rel="stylesheet" />
+      </Head>
       <body>
-        <Navbar/>
+        <Navbar />
         {children}
-        <Footer/>
+        <Footer />
       </body>
     </html>
   );
