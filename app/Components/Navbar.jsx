1d91ab5c7b86dbbac348e65e3c80fadd7ecc3b07
adarshswaminath <adarshswaminath7@gmail.com>
2024-04-22 08:45:52 +0530

    feat: add Link in navabr logo

--- app/Components/Navbar.jsx
+++ app/Components/Navbar.jsx
@@ -5,7 +5,9 @@ function Navbar() {
     return (
         <nav className='navbar p-3 flex items-center justify-between text-white'>
             <div className="flex items-center gap-6">
+                <Link href="/">
                 <h3 className='text-2xl font-bold'>Blog2Buzz</h3>
+                </Link>
                 <div className="flex space-x-6 lg:space-x-12">
                     <Link href="/">Home</Link>
                     <Link href="/#tags">Tags</Link>
