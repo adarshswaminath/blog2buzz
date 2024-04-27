e320b0e27ee1b2c795ff2795fa94a238849c696e
adarshswaminath <adarshswaminath7@gmail.com>
2024-04-27 12:39:08 +0530

    feat: added better styling for the magic link form

--- app/Components/LoginForm.jsx
+++ app/Components/LoginForm.jsx
@@ -1,27 +1,20 @@
 "use client"
 import {Auth} from "@supabase/auth-ui-react"
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs"
+import {ThemeSupa} from "@supabase/auth-ui-shared"
 function LoginForm() {
-  const supabase = createClientComponentClient()
+  const supabaseClient = createClientComponentClient()
   return (
     <main className="flex flex-col h-72 w-96 p-3 justify-center bg-background rounded-lg">
        <Auth
-        supabaseClient={supabase}
+        supabaseClient={supabaseClient}
         view="magic_link"
         showLinks={false}
         providers={[]}
         redirectTo='http://localhost:3000/auth/callback'
-        appearance={{
-            theme: 'dark',
-            button: {
-                className: 'bg-white btn'
-            },
-            input: {
-                className: 'bg-gray-700 border-gray-600 text-white'
-            }
-        }}/>
+        appearance={{ theme: ThemeSupa}}/>
     </main>
   )
 }
 
-export default LoginForm
+export default LoginForm 
