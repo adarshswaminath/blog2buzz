e737110072680003aa91d28f10fddff70d0670ac
adarshswaminath <adarshswaminath7@gmail.com>
2024-04-27 12:04:10 +0530

    added: signout and need a design change for the auth form

--- app/saved/page.jsx
+++ app/saved/page.jsx
@@ -7,21 +7,26 @@ import { BackgroundGradient } from '../Components/ui/background-gradient';
 
 export default function Page() {
   const [posts, setPosts] = useState([]);
-  
+
   useEffect(() => {
     const data = JSON.parse(localStorage.getItem("posts")) || [];
     console.log(data)
     setPosts(data);
   }, []);
 
-console.log(posts)
+  console.log(posts)
   return (
     <main className="min-h-screen">
-      <h2 className='text-center lg:text-4xl text-3xl font-bold text-white mb-4'><span className='text-gray'>#</span>Your Bucket List</h2>
-     
-        <div className='flex flex-wrap gap-2 justify-center items-center mt-6'>
-          {posts.map((data, index) => (
-            <BackgroundGradient key={index} className="rounded-[22px] max-w-sm p-4 sm:p-10 bg-background">
+      <div className="flex items-center justify-between p-3 px-12">
+        <h2 className='text-center lg:text-4xl text-3xl font-bold text-white mb-4'><span className='text-gray'>#</span>Your Bucket List</h2>
+
+        <form action="/auth/signout" method='POST'>
+          <button className='px-12 py-2 bg-background rounded-xl text-white font-bold hover:bg-gradient-to-br hover:from-purple hover:to-white'>Sign Out</button>
+        </form>
+      </div>
+      <div className='flex flex-wrap gap-2 justify-center items-center mt-6'>
+        {posts.map((data, index) => (
+          <BackgroundGradient key={index} className="rounded-[22px] min-h-96 max-w-sm p-4 sm:p-10 bg-background">
             <Image
               src={data.imageUrl}
               alt="jordans"
@@ -30,21 +35,25 @@ console.log(posts)
               className="object-contain"
             />
             <p className="text-base sm:text-xl text-white mt-4 mb-2 ">
-              Air Jordan 4 Retro Reimagined
+              {data.title}
             </p>
-     
+
             <p className="text-sm text-white">
-              {data.article.slice(0,900)}...
+              {data.article.slice(0, 200)}...
             </p>
-            <a href={`https://dev.to/${data.article_url}`} target="_blank" rel="noopener noreferrer">
-            <button className="px-6 py-2 text-base rounded-full bg-black text-white">
-              <span>Read full Article</span>
-            </button>
-            </a>
+            <div className="flex items-center justify-between mt-3">
+              <a href={`https://dev.to/${data.article_url}`} target="_blank" rel="noopener noreferrer">
+                <button className="px-6 py-2 text-base rounded-full bg-black text-white">
+                  <span>Read full Article</span>
+                </button>
+              </a>
+              {/* button to delete */}
+              <button>Delete</button>
+            </div>
           </BackgroundGradient>
-          ))}
-        </div>
-  
+        ))}
+      </div>
+
     </main>
   );
 }
